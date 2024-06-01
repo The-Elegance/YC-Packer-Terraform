8a1306ca50f9649f28739d6f32c312a42b5fc116
Igor <igor.shaposhnikov@urfu.me>
2024-06-01 23:20:47 +0500

    Updated packer_install.bash

--- packer_install.bash
+++ packer_install.bash
@@ -1,14 +1,14 @@
-wget https://hashicorp-releases.yandexcloud.net/packer/1.10.3/packer_1.10.3_linux_amd64.zip
-
+temp_id=$(echo $RANDOM | md5sum | head -c 20)
+zip_file_name="${temp_id}.zip"
+echo $zip_file_name
+mkdir $temp_id
+cd $temp_id
+wget -O $zip_file_name  https://hashicorp-releases.yandexcloud.net/packer/1.10.3/packer_1.10.3_linux_amd64.zip
 sudo apt install unzip
-unzip packer_1.10.3_linux_amd64.zip
-rm packer_1.10.3_linux_amd64.zip
-rm LICENSE.txt
+unzip $zip_file_name
 sudo apt remove unzip -y
-
-sudo mv ~/packer /usr/local/bin/
-
-clear
-echo "Packer installed."
-
+sudo mv packer /usr/local/bin/
+echo $temp_id
+cd ~
+rm -rf $temp_id
 packer
